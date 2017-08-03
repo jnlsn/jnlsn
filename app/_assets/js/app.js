1c1adb41707d88b8792a395ae994cb16d0b77e01
Jay Nielsen <jayarnielsen@gmail.com>
2017-08-03 14:56:28 -0600

    updated hero

--- app/_assets/js/app.js
+++ app/_assets/js/app.js
@@ -1,13 +1,7 @@
 //= require jquery/dist/jquery
 //= require what-input/dist/what-input
 //= require foundation-sites/dist/js/foundation
-//= require jquery-youtube-background/src/jquery.youtubebackground
 
 $(document).ready(function(){
   $(document).foundation();
-  
-  $('.home-splash').YTPlayer({
-    fitToBackground: true,
-    videoId: 'bsXqCi4zp-s'
-  });
 });
