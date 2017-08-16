c3954c70decef622bd937b18b648c267eff33de0
Jay Nielsen <jayarnielsen@gmail.com>
2017-08-16 14:13:09 -0600

    publish: webpack & browsersync configuration revised
    generated from commit 1f23cf62b8563fed8096db872495d1c31993fb60

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-!function(t){function e(n){if(i[n])return i[n].exports;var o=i[n]={i:n,l:!1,exports:{}};return t[n].call(o.exports,o,o.exports,e),o.l=!0,o.exports}var i={};e.m=t,e.c=i,e.d=function(t,i,n){e.o(t,i)||Object.defineProperty(t,i,{configurable:!1,enumerable:!0,get:n})},e.n=function(t){var i=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(i,"a",i),i},e.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},e.p="",e(e.s=19)}([function(t,e,i){var n,o;/*!
+!function(t){function e(n){if(i[n])return i[n].exports;var o=i[n]={i:n,l:!1,exports:{}};return t[n].call(o.exports,o,o.exports,e),o.l=!0,o.exports}var i={};e.m=t,e.c=i,e.d=function(t,i,n){e.o(t,i)||Object.defineProperty(t,i,{configurable:!1,enumerable:!0,get:n})},e.n=function(t){var i=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(i,"a",i),i},e.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},e.p="/assets",e(e.s=19)}([function(t,e,i){var n,o;/*!
  * jQuery JavaScript Library v3.2.1
  * https://jquery.com/
  *
