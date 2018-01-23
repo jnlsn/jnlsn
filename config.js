381016d427d6309f4adf43157cb8c8e052e3282b
Jay Nielsen <jayarnielsen@gmail.com>
2018-01-23 11:10:51 -0700

    upgraded amethyst

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const { argv } = require('yargs');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const url = require('url');
 
+const jekyllConfig = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf8'));
 const isProduction = !!((argv.env && argv.env.production) || argv.p);
 
 module.exports = {
@@ -28,13 +32,17 @@ module.exports = {
     watcher: !!argv.watch,
   },
   env: Object.assign({ production: isProduction, development: !isProduction }, argv.env),
-  devUrl: 'http://127.0.0.1:4000/',
+  devUrl: 'http://127.0.0.1:4000',
   proxyUrl: 'http://localhost:3000',
   watch: [
     "dist/**/*.html",
   ],
   open: true,
-  publicPath: '/assets/',
+  publicPath: path.join(jekyllConfig.baseurl, '/assets/'),
+}
+
+if(jekyllConfig.baseurl !== "") {
+  module.exports.devUrl = url.resolve(module.exports.devUrl, jekyllConfig.baseurl) + '/';
 }
 
 if (process.env.NODE_ENV === undefined) {
